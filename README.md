a32f327a9cf780e4724bd3ad210b0384e8651a4d
Etette Etok <etokoffice@gmail.com>
2023-06-30 14:10:04 +0100

    portfolio_v1 genesis push

---
+++ README.md
@@ -0,0 +1,2 @@
+# Description
+This is a simple portfolio project that alternates as my resume.
---
+++ js/typed.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#text", {
+    strings: ["Software Engineer", "Backend Web Developer", "Solidity Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+// console.log(typed)
+// typed();
